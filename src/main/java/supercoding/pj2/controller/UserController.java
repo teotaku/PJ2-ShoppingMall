419fc287f084305a6eac3aa81f3bc894400f936a
doyulll <ajin7385@naver.com>
2025-05-06 11:10:51 +0900

    feat: 회원탈퇴 기능 구현

---
+++ src/main/java/supercoding/pj2/controller/UserController.java
@@ -0,0 +1,24 @@
+package supercoding.pj2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import supercoding.pj2.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @DeleteMapping("/me")
+    public ResponseEntity<Void> deleteMe(@AuthenticationPrincipal UserDetails userDetails) {
+        userService.softDelete(userDetails.getUsername());
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/supercoding/pj2/service/UserService.java
@@ -0,0 +1,22 @@
+package supercoding.pj2.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import supercoding.pj2.entity.User;
+import supercoding.pj2.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserService
+{
+    private final UserRepository userRepository;
+
+    public void softDelete(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.setDeleted(true);
+        userRepository.save(user);
+    }
+
+}
