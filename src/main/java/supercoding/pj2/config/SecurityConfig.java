1ee0ae0b1bfc4f918be2c67ca9f3715741e83b94
doyulll <ajin7385@naver.com>
2025-05-22 11:47:39 +0900

    fix:회원가입 수정

--- src/main/java/supercoding/pj2/config/SecurityConfig.java
+++ src/main/java/supercoding/pj2/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/signup",
