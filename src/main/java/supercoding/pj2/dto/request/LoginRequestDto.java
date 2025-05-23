4f1f59727c9c6c8de767205e343131ff512f9f17
doyulll <ajin7385@naver.com>
2025-05-23 14:03:11 +0900

    refactor:dto toEntity추가

--- src/main/java/supercoding/pj2/dto/request/LoginRequestDto.java
+++ src/main/java/supercoding/pj2/dto/request/LoginRequestDto.java
@@ -1,11 +1,14 @@
 package supercoding.pj2.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequestDto {
+    @Schema(description = "사용자 이메일", example = "user@example.com")
     private String email;
+    @Schema(description = "사용자 비밀번호", example = "password1234")
     private String password;
 }
