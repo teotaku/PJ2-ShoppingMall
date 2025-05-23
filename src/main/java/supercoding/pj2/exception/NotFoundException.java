5d349c051577897f0964703703d66918beff6138
doyulll <ajin7385@naver.com>
2025-05-23 16:41:31 +0900

    refactor:exception 커스텀 추가

--- src/main/java/supercoding/pj2/exception/NotFoundException.java
+++ src/main/java/supercoding/pj2/exception/NotFoundException.java
@@ -1,7 +1,9 @@
 package supercoding.pj2.exception;
 
-public class NotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends CustomException {
     public NotFoundException(String message) {
-        super(message);
+        super(message, HttpStatus.NOT_FOUND);
     }
 }
