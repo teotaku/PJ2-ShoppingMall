7576058c4744dc48dff325a52ed82ced09a1dd75
teotaku <dongik9467@naver.com>
2025-06-01 16:22:32 +0900

    test fix

--- src/test/java/supercoding/pj2/Pj2ApplicationTests.java
+++ src/test/java/supercoding/pj2/Pj2ApplicationTests.java
@@ -2,9 +2,13 @@ package supercoding.pj2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import supercoding.pj2.s3.S3Uploader;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Pj2ApplicationTests {
+
 
 	@Test
 	void contextLoads() {
