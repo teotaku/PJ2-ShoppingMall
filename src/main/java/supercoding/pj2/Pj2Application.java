5f77afe0ccaf8ed35e18bd7449b0baa30cb119ea
teotaku <dongik9467@naver.com>
2025-06-01 21:17:54 +0900

    1253

--- src/main/java/supercoding/pj2/Pj2Application.java
+++ src/main/java/supercoding/pj2/Pj2Application.java
@@ -12,20 +12,6 @@ import supercoding.pj2.s3.AwsS3Properties;
 public class Pj2Application {
 
 	public static void main(String[] args) {
-		String accessKey = System.getenv("AWS_ACCESS_KEY");
-		String secretKey = System.getenv("AWS_SECRET_KEY");
-
-		if (accessKey != null) {
-			System.setProperty("AWS_ACCESS_KEY", accessKey);
-		} else {
-			System.out.println(" AWS_ACCESS_KEY 환경변수가 설정되지 않았습니다.");
-		}
-
-		if (secretKey != null) {
-			System.setProperty("AWS_SECRET_KEY", secretKey);
-		} else {
-			System.out.println(" AWS_SECRET_KEY 환경변수가 설정되지 않았습니다.");
-		}
 
 		SpringApplication.run(Pj2Application.class, args);
 	}
