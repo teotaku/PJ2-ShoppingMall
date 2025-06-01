d235855ff8333cd6cbf372de38803b9f198d706c
teotaku <dongik9467@naver.com>
2025-06-01 17:22:23 +0900

    1251

--- src/test/java/supercoding/pj2/Pj2ApplicationTests.java
+++ src/test/java/supercoding/pj2/Pj2ApplicationTests.java
@@ -1,8 +1,11 @@
 package supercoding.pj2;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import supercoding.pj2.s3.S3Uploader;
 
@@ -11,9 +14,14 @@ import supercoding.pj2.s3.S3Uploader;
 class Pj2ApplicationTests {
 
 
-
 	@Test
 	void contextLoads() {
 	}
-
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public S3Uploader s3Uploader() {
+			return Mockito.mock(S3Uploader.class);
+		}
+	}
 }
