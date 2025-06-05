48e0fb2e3053e6679cd3e7471240a1852924770f
teotaku <dongik9467@naver.com>
2025-06-05 16:50:28 +0900

    125

--- src/main/java/supercoding/pj2/controller/BasicController.java
+++ src/main/java/supercoding/pj2/controller/BasicController.java
@@ -8,6 +8,6 @@ public class BasicController {
 
     @GetMapping("/")
     public String index() {
-        return "쇼핑몰 2차 프로젝트2  INDEX화면. 스웨거 - > http://52.79.184.1:8080/swagger-ui/index.html";
+        return "쇼핑몰 2차 프로젝트23  INDEX화면. 스웨거 - > http://52.79.184.1:8080/swagger-ui/index.html";
     }
 }
